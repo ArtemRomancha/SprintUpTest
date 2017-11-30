b1ac322b8b8edf8d24e3dc58d70a0042fd185b65
AstureS <a.romancha@hotmail.com>
2017-11-30 19:59:38 +0200

    Create controller for home page

---
+++ src/main/java/app/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Class for mapping home
+ */
+@Controller
+public class HomeController {
+
+    /**
+     * Show home page
+     * @return view
+     */
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
